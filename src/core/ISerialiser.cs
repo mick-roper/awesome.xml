3c3135b4ef1fa56d8c8ccc42dd9d9e360fe8180f
Mick Roper <michael.roper@drax.com>
2018-06-25 08:03:11 +0100

    added serialiser

---
+++ src/core/ISerialiser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Awesome.Xml
+{
+    public interface ISerialiser
+    {
+        string Serialise<T>(T instance) where T : class;
+        T Deserialise<T>(string data) where T : class;
+    }
+}
---
+++ src/core/Serialiser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Awesome.Xml
+{
+    public sealed class Serialiser : ISerialiser
+    {
+        public T Deserialise<T>(string data) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialise<T>(T instance) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
